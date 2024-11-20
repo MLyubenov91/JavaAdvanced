c18e42848decbea9d95612c064d3b179e2ce1cec
MLyubenov91 <mlyubenov11@e-dnrs.org>
2024-11-20 19:57:20 +0200

    added tests for add method of Calculator class

--- FirstStepsWithUnitTesting/src/test/java/CalculatorTest.java
+++ FirstStepsWithUnitTesting/src/test/java/CalculatorTest.java
@@ -1,6 +1,36 @@
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 
 class CalculatorTest {
-    
+
+    @Test
+    public void twoPlusTwoShouldBeFour() {
+        Calculator calculator = new Calculator();
+        int result = calculator.add(2, 2);
+        assertEquals(4, result);
+    }
+
+    @Test
+    public void twoPlusFiveShouldBeSeven() {
+        Calculator calculator = new Calculator();
+        int result = calculator.add(2, 5);
+        assertEquals(7, result);
+    }
+
+    @Test
+    public void twoPlusMinusFiveShouldBeMinusThree() {
+        Calculator calculator = new Calculator();
+        int result = calculator.add(2, -5);
+        assertEquals(-3, result);
+    }
+
+    @Test
+    public void twoPlusZeroShouldBeTwo() {
+        Calculator calculator = new Calculator();
+        int result = calculator.add(2, 0);
+        assertEquals(2, result);
+    }
 
 }
