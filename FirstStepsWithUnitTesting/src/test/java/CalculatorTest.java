99375557ab6cd5d6d7240ded6cc256f0951059fb
MLyubenov91 <mlyubenov11@e-dnrs.org>
2024-11-20 20:05:03 +0200

    rewrited tests by adding BeforeEach method

--- FirstStepsWithUnitTesting/src/test/java/CalculatorTest.java
+++ FirstStepsWithUnitTesting/src/test/java/CalculatorTest.java
@@ -1,3 +1,4 @@
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -5,30 +6,33 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class CalculatorTest {
 
+    private Calculator calculator;
+
+    @BeforeEach
+    void setup() {
+        calculator = new Calculator();
+    }
+
     @Test
     public void twoPlusTwoShouldBeFour() {
-        Calculator calculator = new Calculator();
         int result = calculator.add(2, 2);
         assertEquals(4, result);
     }
 
     @Test
     public void twoPlusFiveShouldBeSeven() {
-        Calculator calculator = new Calculator();
         int result = calculator.add(2, 5);
         assertEquals(7, result);
     }
 
     @Test
     public void twoPlusMinusFiveShouldBeMinusThree() {
-        Calculator calculator = new Calculator();
         int result = calculator.add(2, -5);
         assertEquals(-3, result);
     }
 
     @Test
     public void twoPlusZeroShouldBeTwo() {
-        Calculator calculator = new Calculator();
         int result = calculator.add(2, 0);
         assertEquals(2, result);
     }
