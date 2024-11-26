f98f4d0be70ff0cfb4e43b49d24a380b18efb35c
MLyubenov91 <mlyubenov11@e-dnrs.org>
2024-11-26 19:26:35 +0200

    write RepeatStringTest class

--- FirstStepsWithUnitTesting/src/main/java/RepeatStrings.java
+++ FirstStepsWithUnitTesting/src/main/java/RepeatStrings.java
@@ -1,3 +1,11 @@
 public class RepeatStrings {
+    public String repeat(String[] input) {
+        StringBuilder result = new StringBuilder();
 
+        for (String word : input) {
+            int repeatCount = word.length();
+            result.append(word.repeat(repeatCount));
+        }
+        return result.toString().trim();
+    }
 }
