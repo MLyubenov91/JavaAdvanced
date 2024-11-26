db613fb4b2bdfe164cbb63b72f9abcf6635cc672
MLyubenov91 <mlyubenov11@e-dnrs.org>
2024-11-26 19:55:53 +0200

    added tests in RepeatStringsTest

--- FirstStepsWithUnitTesting/src/test/java/RepeatStringsTest.java
+++ FirstStepsWithUnitTesting/src/test/java/RepeatStringsTest.java
@@ -1,5 +1,35 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class RepeatStringsTest {
-    
+
+    private RepeatStrings repeater;
+
+    @BeforeEach
+    void setup() {
+        repeater = new RepeatStrings();
+    }
+
+    @Test
+    public void helloShouldBeRepeatedFiveTimes() {
+        String[] input = {"Hello"};
+        var result = repeater.repeat(input);
+        assertEquals("HelloHelloHelloHelloHello", result);
+    }
+
+    @Test
+    public void arrayOfThreeOneLetterWordsShouldBeOneThreeLetterWord() {
+        String[] input = {"a", "b", "c"};
+        var result = repeater.repeat(input);
+        assertEquals("abc", result);
+    }
+
+    @Test
+    public void emptyStringShouldNotBeRepeated() {
+        String[] input = {"a", "", "c"};
+        var result = repeater.repeat(input);
+        assertEquals("ac", result);
+    }
 }
