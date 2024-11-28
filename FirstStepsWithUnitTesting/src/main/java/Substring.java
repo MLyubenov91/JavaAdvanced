84281270806300a7d1435a56751d3c654586a701
MLyubenov91 <mlyubenov11@e-dnrs.org>
2024-11-28 17:49:45 +0200

    added class Substring

---
+++ FirstStepsWithUnitTesting/src/main/java/Substring.java
@@ -0,0 +1,8 @@
+public class Substring {
+    public static String removeOccurrences(String toRemove, String input) {
+        
+        input = input.replace(toRemove, "");
+
+        return input.trim();
+    }
+}
