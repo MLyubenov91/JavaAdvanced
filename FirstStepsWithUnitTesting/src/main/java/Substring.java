c0538813325632b468b4ff421abef76cf3b2fa73
MLyubenov91 <mlyubenov11@e-dnrs.org>
2024-11-28 17:50:47 +0200

    added class SubstringTest

--- FirstStepsWithUnitTesting/src/main/java/Substring.java
+++ FirstStepsWithUnitTesting/src/main/java/Substring.java
@@ -1,6 +1,6 @@
 public class Substring {
     public static String removeOccurrences(String toRemove, String input) {
-        
+
         input = input.replace(toRemove, "");
 
         return input.trim();
