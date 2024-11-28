a9388d6534e14452e7980897ffb33d6fb4e33332
MLyubenov91 <mlyubenov11@e-dnrs.org>
2024-11-28 18:26:51 +0200

    added more tests for Substring method in SubstringTest class

--- FirstStepsWithUnitTesting/src/test/java/SubstringTest.java
+++ FirstStepsWithUnitTesting/src/test/java/SubstringTest.java
@@ -12,4 +12,42 @@ class SubstringTest {
         assertEquals("The quick brown jumps over the lazy dog.", result);
     }
 
+    @Test
+    public void Test_removeOccurrences_RemovesSubstringFromStart() {
+        String text = "The quick brown fox jumps over the lazy dog.";
+        String toRemove = "The";
+        String result = Substring.removeOccurrences(toRemove, text);
+        assertEquals("quick brown fox jumps over the lazy dog.", result);
+    }
+
+    @Test
+    public void Test_removeOccurrences_RemovesSubstringFromEnd() {
+        String text = "The quick brown fox jumps over the lazy dog.";
+        String toRemove = "dog.";
+        String result = Substring.removeOccurrences(toRemove, text);
+        assertEquals("The quick brown fox jumps over the lazy", result);
+    }
+
+    @Test
+    public void Test_removeOccurrences_RemovesAllOccurrences() {
+        String text = "The quick fox brown fox fox jumps over fox the lazy fox dog.";
+        String toRemove = "fox";
+        String result = Substring.removeOccurrences(toRemove, text);
+        assertEquals("The quick brown jumps over the lazy dog.", result);
+    }
+
+    @Test
+    public void Test_removeOccurrences_ThrowsIllegalArgumentExceptionWhenTextIsNull() {
+        String text = null;
+        String toRemove = "fox";
+        assertThrows(IllegalArgumentException.class, () -> Substring.removeOccurrences(toRemove, text));
+    }
+
+    @Test
+    public void Test_removeOccurrences_ThrowsIllegalArgumentExceptionWhenSubstringIsNull() {
+        String text = "The quick brown fox jumps over the lazy dog.";
+        String toRemove = null;
+        assertThrows(IllegalArgumentException.class, () -> Substring.removeOccurrences(toRemove, text));
+    }
+
 }
