54bdd09dc98900807228df142f69861b489b1e5a
MLyubenov91 <mlyubenov11@e-dnrs.org>
2024-11-28 18:03:38 +0200

    added check if input text or substring are null in Substring class

--- FirstStepsWithUnitTesting/src/main/java/Substring.java
+++ FirstStepsWithUnitTesting/src/main/java/Substring.java
@@ -1,6 +1,10 @@
 public class Substring {
     public static String removeOccurrences(String toRemove, String input) {
 
+        if (toRemove == null || input == null) {
+            throw new IllegalArgumentException("Both substring and text must be non-null.");
+        }
+
         input = input.replace(toRemove, "");
 
         while (input.contains("  ")) {
