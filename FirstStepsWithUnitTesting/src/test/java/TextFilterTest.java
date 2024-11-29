849acf79b6d1b812efbddc455e9c2aa4c5269b7a
MLyubenov91 <mlyubenov11@e-dnrs.org>
2024-11-29 19:01:46 +0200

    added tests in TextFilterTest class

--- FirstStepsWithUnitTesting/src/test/java/TextFilterTest.java
+++ FirstStepsWithUnitTesting/src/test/java/TextFilterTest.java
@@ -1,5 +1,46 @@
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class TextFilterTest {
 
+    @Test
+    public void Test_Filter_WhenNoBannedWords_ShouldReturnOriginalText() {
+        String text = "The quick brown fox jumps over the lazy dog.";
+        String[] bannedWords = {};
+        String result = TextFilter.Filter(bannedWords, text);
+        assertEquals(text, result);
+    }
+
+    @Test
+    public void Test_Filter_WhenBannedWordExists_ShouldReplaceBannedWordWithAsterisks() {
+        String text = "The quick brown fox jumps over the lazy dog.";
+        String[] bannedWords = {"fox"};
+        String result = TextFilter.Filter(bannedWords, text);
+        assertEquals("The quick brown *** jumps over the lazy dog.", result);
+    }
+
+    @Test
+    public void Test_Filter_WhenBannedWordsAreEmpty_ShouldReturnOriginalText() {
+        String text = "The quick brown fox jumps over the lazy dog.";
+        String[] bannedWords = {"", "", ""};
+        String result = TextFilter.Filter(bannedWords, text);
+        assertEquals(text, result);
+    }
+
+    @Test
+    public void Test_Filter_WhenBannedWordsContainWhiteSpace_ShouldReplaceBannedWord() {
+        String text = "The quick brown fox jumps over the lazy dog.";
+        String[] bannedWords = {"brown fox"};
+        String result = TextFilter.Filter(bannedWords, text);
+        assertEquals("The quick ********* jumps over the lazy dog.", result);
+    }
+
+    @Test
+    public void Test_Filter_WhenBannedWordExists_ShouldReplaceAllOccurrencesOfBannedWord() {
+        String text = "The fox quick brown fox jumps over the fox lazy dog.";
+        String[] bannedWords = {"fox"};
+        String result = TextFilter.Filter(bannedWords, text);
+        assertEquals("The *** quick brown *** jumps over the *** lazy dog.", result);
+    }
 }
