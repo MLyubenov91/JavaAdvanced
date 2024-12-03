981724912aa77ea8223b671473aa6f5a95b3ff69
MLyubenov91 <mlyubenov11@e-dnrs.org>
2024-12-03 22:18:37 +0200

    added Test_EmptyInput_ReturnsEmptyString in ReverseAndConcatenateTest class

--- FirstStepsWithUnitTesting/src/test/java/ReverseAndConcatenateTest.java
+++ FirstStepsWithUnitTesting/src/test/java/ReverseAndConcatenateTest.java
@@ -1,5 +1,14 @@
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class ReverseAndConcatenateTest {
+
+    @Test
+    public void Test_EmptyInput_ReturnsEmptyString() {
+        String[] input = {};
+        String result = ReverseAndConcatenate.ReverseAndConcatenateStrings(input);
+        assertEquals("", result);
+    }
 
 }
