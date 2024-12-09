08d1c0372ce2d343af0059d4a7ea341f3da30ef3
MLyubenov91 <mlyubenov11@e-dnrs.org>
2024-12-09 17:59:53 +0200

    added MatchNames class

---
+++ FirstStepsWithUnitTesting/src/main/java/MatchNames.java
@@ -0,0 +1,17 @@
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class MatchNames {
+    public static String Match(String names) {
+        String regex = "\\b[A-Z][a-z]+ [A-Z][a-z]+\\b";
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(names);
+
+        StringBuilder sb = new StringBuilder();
+        while (matcher.find()) {
+            sb.append(matcher.group()).append(" ");
+        }
+
+        return sb.toString().trim();
+    }
+}
