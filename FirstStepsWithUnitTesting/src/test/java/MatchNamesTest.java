2eacb3563d9c20ef8f427292228bf9740b2317d0
MLyubenov91 <mlyubenov11@e-dnrs.org>
2024-12-10 17:55:34 +0200

    added Test_Match_ValidNames_ReturnsMatchedNames in MatchNamesTest class

--- FirstStepsWithUnitTesting/src/test/java/MatchNamesTest.java
+++ FirstStepsWithUnitTesting/src/test/java/MatchNamesTest.java
@@ -1,5 +1,13 @@
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class MatchNamesTest {
 
+    @Test
+    public void Test_Match_ValidNames_ReturnsMatchedNames() {
+        String input = "John Doe lives on James Johnson blvd.";
+        String result = MatchNames.Match(input);
+        assertEquals("John Doe James Johnson", result);
+    }
 }
