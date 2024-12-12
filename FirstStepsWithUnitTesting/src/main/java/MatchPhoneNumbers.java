6279cf45c0e5d5bfaac3595bef88162799a1906b
MLyubenov91 <mlyubenov11@e-dnrs.org>
2024-12-12 20:00:16 +0200

    implemented Match method in MatchPhoneNumbers class

--- FirstStepsWithUnitTesting/src/main/java/MatchPhoneNumbers.java
+++ FirstStepsWithUnitTesting/src/main/java/MatchPhoneNumbers.java
@@ -1,4 +1,18 @@
-package PACKAGE_NAME;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class MatchPhoneNumbers {
+    public static String Match(String text) {
+        String regex = "\\+359 2 [0-9]{3} [0-9]{4}|\\+359-2-[0-9]{3}-[0-9]{4}\\b";
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(text);
+
+        StringBuilder validPhones = new StringBuilder();
+
+        while (matcher.find()) {
+            validPhones.append(matcher.group()).append(", ");
+        }
+
+        return validPhones.toString().trim();
+    }
 }
