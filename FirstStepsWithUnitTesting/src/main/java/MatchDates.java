938a750d2f407a60ea73d5a2a035bc8f0d1bf63d
MLyubenov91 <mlyubenov11@e-dnrs.org>
2024-12-16 19:57:13 +0200

    added first two tests in MatchDatesTest class

--- FirstStepsWithUnitTesting/src/main/java/MatchDates.java
+++ FirstStepsWithUnitTesting/src/main/java/MatchDates.java
@@ -7,18 +7,20 @@ public class MatchDates {
             throw new IllegalArgumentException("Input cannot be null!");
         }
 
-        String regex = "@\\b(?<day>\\d{2})(?<separator>[-./])(?<month>[A-Z][a-z]+)\\k<separator>(?<year>\\d{4})";
+        String regex = "\\b(?<day>\\d{2})(?<separator>[-./])(?<month>[A-Z][a-z]+)\\k<separator>(?<year>\\d{4})";
         Pattern pattern = Pattern.compile(regex);
         Matcher matcher = pattern.matcher(dates);
+
+        StringBuilder result = new StringBuilder();
 
         while (matcher.find()) {
             String day = matcher.group("day");
             String month = matcher.group("month");
             String year = matcher.group("year");
 
-            return String.format("Day: %s, Month: %s, Year: %s", day, month, year);
+            result.append(String.format("Day: %s, Month: %s, Year: %s\n", day, month, year));
         }
 
-        return "";
+        return !result.isEmpty() ? result.toString().trim() : "";
     }
 }
--- FirstStepsWithUnitTesting/src/test/java/MatchDatesTest.java
+++ FirstStepsWithUnitTesting/src/test/java/MatchDatesTest.java
@@ -1,5 +1,21 @@
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class MatchDatesTest {
+
+    @Test
+    public void Test_Match_ValidDate_ReturnsExpectedResult(){
+        String input = "11-April-1970";
+        String result = MatchDates.Match(input);
+        assertEquals("Day: 11, Month: April, Year: 1970", result);
+    }
+
+    @Test
+    public void Test_Match_MultipleMatches_ReturnsExpectedResult(){
+        String input = "11-April-1970 31.December.1991";
+        String result = MatchDates.Match(input);
+        assertEquals("Day: 11, Month: April, Year: 1970\nDay: 31, Month: December, Year: 1991", result);
+    }
 
 }
