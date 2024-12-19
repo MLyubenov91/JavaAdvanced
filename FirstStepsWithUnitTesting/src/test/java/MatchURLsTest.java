ebc9216f8c95d98d21b4710cc2f7bc42cc7abfa1
MLyubenov91 <mlyubenov11@e-dnrs.org>
2024-12-19 19:11:07 +0200

    added Test_ExtractUrls_EmptyText_ReturnsEmptyList in MatchURLsTest class

--- FirstStepsWithUnitTesting/src/test/java/MatchURLsTest.java
+++ FirstStepsWithUnitTesting/src/test/java/MatchURLsTest.java
@@ -1,5 +1,16 @@
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class MatchURLsTest {
+
+    @Test
+    public void Test_ExtractUrls_EmptyText_ReturnsEmptyList() {
+        String text = "";
+        List<String> result = MatchURLs.ExtractUrls(text);
+        assertTrue(result.isEmpty());
+    }
 
 }
