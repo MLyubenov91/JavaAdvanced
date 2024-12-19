bc26676246467fd0873aaec4f27e70efaefa7441
MLyubenov91 <mlyubenov11@e-dnrs.org>
2024-12-19 19:03:42 +0200

    implemented MatchURLs class

--- FirstStepsWithUnitTesting/src/main/java/MatchURLs.java
+++ FirstStepsWithUnitTesting/src/main/java/MatchURLs.java
@@ -1,2 +1,19 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class MatchURLs {
+    public static List<String> ExtractUrls(String text) {
+        String regex = "https?:\\/\\/(www\\.)?[-a-zA-Z-0-9@:%._\\+~#=]{1,256}\\.[a-zA-Z0-9()]{1,6}\\b([-a-zA-Z-0-9@:%._\\+~#=]*)";
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(text);
+
+        List<String> urls = new ArrayList<>();
+        while (matcher.find()) {
+            urls.add(matcher.group());
+        }
+
+        return urls;
+    }
 }
